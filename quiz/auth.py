3385d6b713c2305850d4683e7147a750484320fb
paul meyers <pmkundika@gmail.com>
2025-10-17 19:40:06 +0200

    Views and URLS

---
+++ quiz/auth.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponseNotFound, HttpResponse
+from django.contrib.auth import login, logout, authenticate, get_user_model
+
+User = get_user_model()
+
+def login_view(request):
+    pass
+
+def logout_view(request):
+    pass
--- quiz/urls.py
+++ quiz/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, auth, student_view, teacher_view
+
+app_name = 'quiz'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login/', auth.login_view, name='login'),
+    path('logout/', auth.logout_view, name='logout')
 ]
