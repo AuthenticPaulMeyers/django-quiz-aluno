53e6bb2e78b455f917de07fefbe184d822c7e5b7
paul meyers <pmkundika@gmail.com>
2025-10-17 20:06:34 +0200

    Student views and urls

--- quiz/auth.py
+++ quiz/auth.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponseNotFound, HttpResponse
 from django.contrib.auth import login, logout, authenticate, get_user_model
 
+# define the custom user model
 User = get_user_model()
 
 def login_view(request):
---
+++ quiz/student_view.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest, HttpResponseNotFound, HttpResponse
+
+def student_dashboard_view(request):
+    pass
+
+def all_quizzes_view(request):
+    pass
+
+def quiz_history_view(request):
+    pass
+
+def student_profile_view(request):
+    pass
+
+def quiz_details_view(request):
+    pass
+
+def attempt_quiz_view(request):
+    pass
+
+def quiz_results_view(request):
+    pass
+
