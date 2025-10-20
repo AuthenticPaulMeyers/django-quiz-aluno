20621687974425775ec64ab1bd36eb65c9729635
paul meyers <pmkundika@gmail.com>
2025-10-20 19:13:45 +0200

    Rendered data on the dashboard

--- quiz/student_view.py
+++ quiz/student_view.py
@@ -17,21 +17,44 @@ def student_dashboard_view(request):
             class_quizzes = Quiz.objects.filter(quizclass=class_enrolled.id).all()
             
             if not class_quizzes:
-                return render(request, 'students/student-dashboard.html', {'message': 'No active quizzes available'})
+                return render(request, 'students/student-dashboard.html', {'message': 'You do not have active quizzes.'})
             
             context = {
                 'user': user,
                 'all_quizzes': class_quizzes
             }
+
         except Student.DoesNotExist:
             print("Student does not exist.")
 
-        return render(request, 'students/student-dashboard.html', context=context)
+        return render(request, 'students/student-dashboard.html', context)
 
 def all_quizzes_view(request):
-    return render(request, 'students/view-all-quizzes.html')
+    if request.user.is_authenticated:
+        user = request.user
 
-def quiz_history_view(request):
+        try:
+            student_obj = user.student
+            # get the class the current user is enrolled in
+            class_enrolled = student_obj.class_enrolled
+
+            # get the quizzes all quizzes for the current user class
+            class_quizzes = Quiz.objects.filter(quizclass=class_enrolled.id).all()
+            
+            if not class_quizzes:
+                return render(request, 'students/view-all-quizzes.html', {'message': 'You do not have active quizzes.'})
+            
+            context = {
+                'user': user,
+                'all_quizzes': class_quizzes
+            }
+
+        except Student.DoesNotExist:
+            print("Student does not exist.")
+
+    return render(request, 'students/view-all-quizzes.html', context)
+
+def quiz_history_view(request, student_id):
     return render(request, 'students/quiz-history.html')
 
 def quiz_details_view(request, quiz_id):
@@ -44,13 +67,11 @@ def quiz_details_view(request, quiz_id):
     }
     return render(request, 'students/quiz-details.html', context=context)
 
-def attempt_quiz_view(request):
+def attempt_quiz_view(request, quiz_id):
     return render(request, 'students/quiz-page.html')
 
 def quiz_results_view(request):
     return render(request, 'students/quiz-results.html')
 
-def student_profile_view(request):
-    return render(request, 'students/quiz-history.html')
-
-
+def student_profile_view(request, student_id):
+    return render(request, 'students/student-profile.html')
