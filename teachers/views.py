c56a1e4ba44bffd843cb3c59ebe0f0dc68ef9427
paul meyers <pmkundika@gmail.com>
2025-10-30 08:04:05 +0200

    Added urls to templates

--- teachers/views.py
+++ teachers/views.py
@@ -23,6 +23,38 @@ def all_quizzes_view(request):
             'user': user,
             'title': 'All Quizzes',
       }
-      return render(request, 'teachers/all_quizzes.html', context)
+      return render(request, 'teachers/all-quizzes.html', context)
 
+#view classes
+@login_required(login_url='quiz:login')
+def classes_view(request):
+      user = request.user
+
+      context={
+            'user': user,
+            'title': 'Classes'
+      }
+      return render(request, 'teachers/classes.html', context)
+
+#view reports
+@login_required(login_url='quiz:login')
+def reports_view(request):
+      user = request.user
+
+      context={
+            'user': user,
+            'title': 'Reports'
+      }
+      return render(request, 'teachers/reports.html', context)
+
+#view reports
+@login_required(login_url='quiz:login')
+def subjects_view(request):
+      user = request.user
+
+      context={
+            'user': user,
+            'title': 'Subjects'
+      }
+      return render(request, 'teachers/subjects.html', context)
 
