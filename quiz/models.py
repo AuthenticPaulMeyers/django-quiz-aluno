2103ed0e76f03b9fac68f744ea977baf87d05a75
paul meyers <pmkundika@gmail.com>
2025-12-24 04:21:49 +0200

    Separated  teacher models to the teachers app

--- quiz/models.py
+++ quiz/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from ..teachers.models import TeacherSubjectClass
 # Create your models here.
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
@@ -25,59 +25,6 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
     
-class Teacher(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.user.first_name} {self.user.last_name}'
-    
-    def fullname(self):
-        return f'{self.user.first_name} {self.user.last_name}'
-    
-    # get subjects taught by this teacher
-    def get_subjects_class(self):
-        from django.db.models import Q
-        return TeacherSubjectClass.objects.filter(
-            Q(subject_teacher__teacher=self)
-        ).distinct()
-    
-    # get number of subjects taught by this teacher
-    def subjects_count(self):
-        return self.get_subjects_class().count()
-    
-    # get classes taught by this teacher
-    def get_classes(self):
-        from django.db.models import Q
-        return Class.objects.filter(
-            Q(teachersubjectclass__subject_teacher__teacher=self)
-        ).distinct()
-    
-    # get number of classes taught by this teacher
-    def classes_count(self):
-        return self.get_classes().count()
-    
-    # get quizzes created by this teacher
-    def get_quizzes(self):
-        from django.db.models import Q
-        return Quiz.objects.filter(
-            Q(teacher_subject_class__subject_teacher__teacher=self)
-        ).distinct()
-    
-    # get number of quizzes created by this teacher
-    def quizzes_count(self):
-        return self.get_quizzes().count()
-    
-    # get students taught by this teacher
-    def get_students(self):
-        from django.db.models import Q
-        return Student.objects.filter(
-            Q(class_enrolled__teachersubjectclass__subject_teacher__teacher=self)
-        ).distinct()
-    
-    # get number of students taught by this teacher
-    def students_count(self):
-        return self.get_students().count()
-
 class Class(models.Model):
     name = models.CharField(max_length=30, null=False, unique=True)
 
@@ -263,40 +210,6 @@ class StudentSubject(models.Model):
     def quizzes_count(self):
         return self.get_quizzes().count()
     
-class SubjectTeacher(models.Model):
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.teacher.fullname()} teaches {self.subject.name}'
-    
-class TeacherSubjectClass(models.Model):
-    subject_teacher = models.ForeignKey(SubjectTeacher, on_delete=models.CASCADE)
-    classname = models.ForeignKey(Class, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.subject_teacher.subject.name} - {self.classname.name}'
-    
-    # get quizzes for this teacher subject class
-    def get_quizzes(self):
-        return Quiz.objects.filter(teacher_subject_class=self)
-    
-    # get number of quizzes for this teacher subject class
-    def quizzes_count(self):
-        return self.get_quizzes().count()
-    
-    # get students for this teacher subject class
-    def get_students(self):
-        from django.db.models import Q
-        return Student.objects.filter(
-            Q(class_enrolled=self.classname) &
-            Q(studentsubject__class_subject__subject=self.subject_teacher.subject)
-        ).distinct()
-    
-    # get number of students for this teacher subject class
-    def students_count(self):
-        return self.get_students().count()
-
 class Quiz(models.Model):
     title = models.CharField(max_length=255, null=False)
     description = models.CharField(max_length=255)
--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,89 @@
 from django.db import models
-
+from ..quiz.models import Student, Class, Subject, CustomUser, Quiz
 # Create your models here.
+class Teacher(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+    
+    def fullname(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+    
+    # get subjects taught by this teacher
+    def get_subjects_class(self):
+        from django.db.models import Q
+        return TeacherSubjectClass.objects.filter(
+            Q(subject_teacher__teacher=self)
+        ).distinct()
+    
+    # get number of subjects taught by this teacher
+    def subjects_count(self):
+        return self.get_subjects_class().count()
+    
+    # get classes taught by this teacher
+    def get_classes(self):
+        from django.db.models import Q
+        return Class.objects.filter(
+            Q(teachersubjectclass__subject_teacher__teacher=self)
+        ).distinct()
+    
+    # get number of classes taught by this teacher
+    def classes_count(self):
+        return self.get_classes().count()
+    
+    # get quizzes created by this teacher
+    def get_quizzes(self):
+        from django.db.models import Q
+        return Quiz.objects.filter(
+            Q(teacher_subject_class__subject_teacher__teacher=self)
+        ).distinct()
+    
+    # get number of quizzes created by this teacher
+    def quizzes_count(self):
+        return self.get_quizzes().count()
+    
+    # get students taught by this teacher
+    def get_students(self):
+        from django.db.models import Q
+        return Student.objects.filter(
+            Q(class_enrolled__teachersubjectclass__subject_teacher__teacher=self)
+        ).distinct()
+    
+    # get number of students taught by this teacher
+    def students_count(self):
+        return self.get_students().count()
+
+class SubjectTeacher(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.teacher.fullname()} teaches {self.subject.name}'
+    
+class TeacherSubjectClass(models.Model):
+    subject_teacher = models.ForeignKey(SubjectTeacher, on_delete=models.CASCADE)
+    classname = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.subject_teacher.subject.name} - {self.classname.name}'
+    
+    # get quizzes for this teacher subject class
+    def get_quizzes(self):
+        return Quiz.objects.filter(teacher_subject_class=self)
+    
+    # get number of quizzes for this teacher subject class
+    def quizzes_count(self):
+        return self.get_quizzes().count()
+    
+    # get students for this teacher subject class
+    def get_students(self):
+        from django.db.models import Q
+        return Student.objects.filter(
+            Q(class_enrolled=self.classname) &
+            Q(studentsubject__class_subject__subject=self.subject_teacher.subject)
+        ).distinct()
+    
+    # get number of students for this teacher subject class
+    def students_count(self):
+        return self.get_students().count()
