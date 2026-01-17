858db393fc04c8b72f62059d5bbc1935cc01b03f
paul meyers <pmkundika@gmail.com>
2026-01-17 19:56:15 +0200

    Feat: Disable button on change password to avoid double click

---
+++ quiz/static/js/change-password.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", function (e) {
+      e.preventDefault()
+
+      const form = document.querySelector("form");
+      const btn = document.getElementById("change-password-btn");
+      const spinner = document.getElementById("loading-spinner");
+
+      form.addEventListener("submit", function (e) {
+            e.preventDefault()
+
+            btn.disabled = true;
+            btn.classList.add("opacity-75", "cursor-not-allowed");
+            spinner.classList.remove("hidden");
+      });
+});
